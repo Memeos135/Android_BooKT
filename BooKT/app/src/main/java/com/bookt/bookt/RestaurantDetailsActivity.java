346869c748efb2819df26fd89412666fe27840e9
mohammedbokhari <bookt19.15@gmail.com>
2019-01-29 18:44:33 +0300

    Header CardView, ExpandedListView and Google Maps MapView for RestaurantDetailsActivity

--- BooKT/app/src/main/java/com/bookt/bookt/RestaurantDetailsActivity.java
+++ BooKT/app/src/main/java/com/bookt/bookt/RestaurantDetailsActivity.java
@@ -1,7 +1,8 @@
 package com.bookt.bookt;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,20 +10,104 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.Toast;
 
-public class RestaurantDetailsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+public class RestaurantDetailsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     ImageView locationImageView;
+    ExpandableListView expandableListView;
+    ArrayList<String> groupItem = new ArrayList<String>();
+    ArrayList<Object> childItem = new ArrayList<Object>();
+    MapView mapView;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_details);
+
+        setGroupData();
+        setChildGroupData();
+
+        mapView = findViewById(R.id.mapView2);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+                expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+
+        expandableListView.setFocusable(false);
+
+        ExpandableListViewAdapter mNewAdapter = new ExpandableListViewAdapter(groupItem, childItem);
+        mNewAdapter
+                .setInflater(
+                        (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE),
+                        this);
+        expandableListView.setAdapter(mNewAdapter);
+
+        if (groupItem.size() > 4) {
+            ListAdapter listadp = expandableListView.getAdapter();
+            if (listadp != null) {
+                int totalHeight = 0;
+                for (int i = 0; i < listadp.getCount(); i++) {
+                    View listItem = listadp.getView(i, null, expandableListView);
+                    listItem.measure(0, 0);
+                    totalHeight += listItem.getMeasuredHeight();
+                }
+                ViewGroup.LayoutParams params = expandableListView.getLayoutParams();
+                params.height = totalHeight + (expandableListView.getDividerHeight() * (listadp.getCount() - 1));
+                expandableListView.setLayoutParams(params);
+                expandableListView.requestLayout();
+            }
+        }
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int i) {
+                Toast.makeText(getApplicationContext(), "Clicked On Group Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int i) {
+                Toast.makeText(getApplicationContext(), "Clicked On Group Collapsed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                Toast.makeText(getApplicationContext(), "Clicked On Child",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // ENABLE NESTED SCROLLING OF EXPANDED LIST VIEW
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            expandableListView.setNestedScrollingEnabled(true);
+        }
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,12 +128,13 @@ public class RestaurantDetailsActivity extends AppCompatActivity implements Navi
             public void onClick(View v) {
                 locationImageView.clearAnimation();
                 locationImageView.setRotation(360);
-                locationImageView.animate().rotation(locationImageView.getRotation()+360).setDuration(500);
+                locationImageView.animate().rotation(locationImageView.getRotation() + 360).setDuration(500);
             }
         });
 
 
     }
+
 
     @Override
     public void onBackPressed() {
@@ -79,5 +165,103 @@ public class RestaurantDetailsActivity extends AppCompatActivity implements Navi
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void setGroupData() {
+        groupItem.add("TechNology");
+        groupItem.add("Mobile");
+        groupItem.add("Manufacturer");
+        groupItem.add("Extras");
+        groupItem.add("Science");
+        groupItem.add("Business");
+        groupItem.add("Medicine");
+    }
+
+    public void setChildGroupData() {
+        /**
+         * Add Data For TecthNology
+         */
+        ArrayList<String> child = new ArrayList<String>();
+        child.add("Java");
+        child.add("Drupal");
+        child.add(".Net Framework");
+        child.add("PHP");
+        childItem.add(child);
+
+        /**
+         * Add Data For Mobile
+         */
+        child = new ArrayList<String>();
+        child.add("Android");
+        child.add("Window Mobile");
+        child.add("iPHone");
+        child.add("Blackberry");
+        childItem.add(child);
+        /**
+         * Add Data For Manufacture
+         */
+        child = new ArrayList<String>();
+        child.add("HTC");
+        child.add("Apple");
+        child.add("Samsung");
+        child.add("Nokia");
+        childItem.add(child);
+        /**
+         * Add Data For Extras
+         */
+        child = new ArrayList<String>();
+        child.add("Contact Us");
+        child.add("About Us");
+        child.add("Location");
+        child.add("Root Cause");
+        childItem.add(child);
+        /**
+         * Add Data For Science
+         */
+        child = new ArrayList<String>();
+        child.add("Contact Us");
+        child.add("About Us");
+        child.add("Location");
+        child.add("Root Cause");
+        childItem.add(child);
+        /**
+         * Add Data For Business
+         */
+        child = new ArrayList<String>();
+        child.add("Contact Us");
+        child.add("About Us");
+        child.add("Location");
+        child.add("Root Cause");
+        childItem.add(child);
+        /**
+         * Add Data For Medicine
+         */
+        child = new ArrayList<String>();
+        child.add("Contact Us");
+        child.add("About Us");
+        child.add("Location");
+        child.add("Root Cause");
+        childItem.add(child);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(21.802820, 39.132578))      // Sets the center of the map to Mountain View
+                .zoom(15)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        // Used to add Marker to Google Maps
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(cameraPosition.target);
+        mMap.addMarker(markerOptions);
+
     }
 }
