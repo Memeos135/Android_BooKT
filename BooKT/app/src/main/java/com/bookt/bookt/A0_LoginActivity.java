fa89db3001c305f0c315962cd4d5f6f9920ac174
Mohammed <memeos_135@hotmail.com>
2019-03-27 14:19:25 +0300

    Firebase Authentication, Forgot Password, and Users Node.

--- BooKT/app/src/main/java/com/bookt/bookt/A0_LoginActivity.java
+++ BooKT/app/src/main/java/com/bookt/bookt/A0_LoginActivity.java
@@ -3,14 +3,23 @@ package com.bookt.bookt;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,11 +27,24 @@ import java.util.List;
 public class A0_LoginActivity extends AppCompatActivity {
 
     Context context;
+    private TextView emailAddress;
+    private TextView password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a0_login_activity);
+
+        context = this;
+
+        emailAddress = findViewById(R.id.usernameText);
+        password = findViewById(R.id.passwordText);
+
+        if(getIntent().getStringExtra("email") != null){
+            Toast.makeText(context, "Registration Successful. Please proceed by logging in.", Toast.LENGTH_SHORT).show();
+            emailAddress.setText(getIntent().getStringExtra("email"));
+            password.setText(getIntent().getStringExtra("password"));
+        }
 
         ImageView homeImage = findViewById(R.id.homeImage);
         homeImage.setOnClickListener(new View.OnClickListener() {
@@ -31,44 +53,99 @@ public class A0_LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), A1_GalleryActivity.class));
             }
         });
-
-        context = this;
     }
 
     // Sign in Method
     public void signinMethod(View v){
-        // app signin
-        if("loginSignupButton".equals(v.getTag())){
-            Toast.makeText(context, "Firebase Authentication Check", Toast.LENGTH_SHORT).show();
 
-            // google signin
-        }else if("googleLogin".equals(v.getTag())){
-            Button button = (Button) v;
+        // forgot password
+        if(v.getTag().equals("forgotPassword")){
 
-            List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+            if(emailAddress.getText().toString().equals("")){
+                Toast.makeText(context, "Please provide an email address.", Toast.LENGTH_SHORT).show();
+            }else{
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.sendPasswordResetEmail(emailAddress.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(context, "A password reset link has been sent to your email address.", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(context, "Password reset email failed to send.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
 
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(),
-                    100);
+        }else {
+                // app signin
+                if ("loginSignupButton".equals(v.getTag())) {
+                    if (!emailAddress.getText().toString().equals("") && !password.getText().toString().equals("")) {
+                        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-            // facebook signin
-        }else if("facebookLogin".equals(v.getTag())){
-            Button button = (Button) v;
+                        mAuth.signInWithEmailAndPassword(emailAddress.getText().toString(), password.getText().toString())
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
 
-            List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.FacebookBuilder().build());
+                                            Toast.makeText(context, "LOGIN WORKS", Toast.LENGTH_SHORT).show();
+                                            FirebaseAuth.getInstance().signOut();
 
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(providers)
-                            .build(),
-                    100);
-            // forgot password
-        }else{
-            Toast.makeText(context, "Forgot Password Dialog", Toast.LENGTH_SHORT).show();
+                                        } else {
+
+                                            Toast.makeText(context, "Authentication failed, please try again.", Toast.LENGTH_SHORT).show();
+
+                                        }
+                                    }
+                                });
+                    }else{
+                        Toast.makeText(context, "Please fill in all required fields.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    // google signin
+                } else if ("googleLogin".equals(v.getTag())) {
+                    Button button = (Button) v;
+
+                    List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            100);
+
+                    // facebook signin
+                } else if ("facebookLogin".equals(v.getTag())) {
+                    Button button = (Button) v;
+
+                    List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.FacebookBuilder().build());
+
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            100);
+                }
+            }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(requestCode == RESULT_OK){
+                Toast.makeText(context, "Successful signin", Toast.LENGTH_SHORT).show();
+//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            }else{
+                Toast.makeText(context, response.getError().toString(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
--- BooKT/app/src/main/java/com/bookt/bookt/A0_SignupActivity.java
+++ BooKT/app/src/main/java/com/bookt/bookt/A0_SignupActivity.java
@@ -2,17 +2,35 @@ package com.bookt.bookt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class A0_SignupActivity extends AppCompatActivity {
+    private TextView emailAddress;
+    private TextView personName;
+    private TextView password;
+    private TextView personMobile;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a0_signup_activity);
+
+        emailAddress = findViewById(R.id.emailText);
+        personName = findViewById(R.id.nameText);
+        password = findViewById(R.id.passwordText);
+        personMobile = findViewById(R.id.mobileText);
 
         ImageView homeImage = findViewById(R.id.homeImage);
         homeImage.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +42,40 @@ public class A0_SignupActivity extends AppCompatActivity {
     }
 
     public void signupHandler(View view){
-        Toast.makeText(this, "Firebase Authentication Next!", Toast.LENGTH_SHORT).show();
+        if(personMobile.getText().equals("") || emailAddress.getText().equals("") || password.getText().equals("")
+         || personMobile.getText().equals("") || (personMobile.getText().toString().length() > 10)){
+
+            Toast.makeText(this, "Please fill in all fields. Mobile numbers cannot exceed 10 digits.", Toast.LENGTH_SHORT).show();
+
+        }else{
+
+            final FirebaseAuth mAuth  = FirebaseAuth.getInstance();
+
+            mAuth.createUserWithEmailAndPassword(emailAddress.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+
+                        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("Users").child(String.valueOf(mAuth.getUid()));
+                        UserProfileInformation userProfileInformation = new UserProfileInformation(personName.getText().toString(),
+                                emailAddress.getText().toString(), personMobile.getText().toString());
+                        mDatabase.child("profile_info").setValue(userProfileInformation);
+                        mDatabase.child("reservations").child("active").setValue("empty");
+                        mDatabase.child("reservations").child("in-active").setValue("empty");
+
+                        startActivity(new Intent(getApplicationContext(), A0_LoginActivity.class)
+                        .putExtra("email", emailAddress.getText().toString())
+                        .putExtra("password", password.getText().toString()));
+
+                    }else{
+
+                        Toast.makeText(A0_SignupActivity.this, "Registration failed, please try again.", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+        }
     }
 }
